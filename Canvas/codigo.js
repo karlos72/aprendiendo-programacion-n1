fa6f3a976696ebbb867dd8055abff210423113a7
fenixdorad0 <fenix0160@hotmail.com>
2020-01-22 15:15:11 -0500

    Aprender programacion n 2

--- Canvas/codigo.js
+++ Canvas/codigo.js
@@ -1,9 +1,54 @@
 var d=document.getElementById("dibujito");
 var Lienzo = d.getContext("2d");
-console.log(Lienzo);
-Lienzo.beginPath();
-Lienzo.strokeStyle="blue";
-Lienzo.moveTo(250,50);
-Lienzo.lineTo(0,300);
-Lienzo.stroke();
-Lienzo.closePath();
+var contador =0;
+var lineas =50;
+var contadorx,contadory=0;
+
+
+while(contador< lineas)
+{
+    contadory=10*contador;
+    contadorx=10*(contador+1);
+    dibujarLinea("blue",0,contadory,contadorx,500);
+    
+    console.log("linea"+contador);
+    contador = contador+1;
+}
+
+var contadorxi=0;//aumenta
+var contadoryf=500;//dismuye
+contador=0;
+
+while(contador<lineas)
+{
+    contadorxi=contadorxi+10;
+    contadoryf=contadoryf-10;
+    dibujarLinea("Green",contadorxi,500,500,contadoryf)
+    contador=contador+1;
+
+}
+
+var contadorxi=0;//aumenta
+var contadoryf=500;//dismuye
+contador=0;
+
+while(contador<lineas)
+{
+    contadorxi=contadorxi+10;
+    contadoryf=contadoryf-10;
+    dibujarLinea("Green",contadorxi,500,500,contadoryf)
+    contador=contador+1;
+
+}
+
+
+function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
+    {
+        Lienzo.beginPath();
+    Lienzo.strokeStyle=color;
+    Lienzo.moveTo(xinicial,yinicial);
+    Lienzo.lineTo(xfinal,yfinal);
+    Lienzo.stroke();
+    Lienzo.closePath();
+
+    }
